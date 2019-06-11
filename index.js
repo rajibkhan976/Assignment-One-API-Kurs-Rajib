b7993cc8d2815dc9e776defeeb2bbf3baf167961
unknown <rajibkhan976@gmail.com>
2019-06-11 11:20:50 +0200

    Test implemented

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const routes = require('./routes');
 const db = require('./models');
-const app = express();
+const app = express(); 
 
 // environment variable PORT or 5000 if unset
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use((error, req, res, next) => {
+	if (res.headersSent) {
+		return next(err);
+	}
+	res.status(error.statusCode || error.status || 500).send({error: error});
+})
 
 app.use((req, res, next) => {
 	req.models = db.models;
@@ -18,10 +29,14 @@ app.use((req, res, next) => {
 app.use('/', routes);
 
 // Start up server and begin listen to requests
-db.connectDB().then(() => {
+if (process.env.NODE_ENV != 'test') {
+	db.connectDB().then(() => {
 	const listener = app.listen(port, () => {
 		console.info(`Server is listening on port ${port}.`);
 	})
 }).catch((error) => {
 	console.error(error);
 })
+}
+
+module.exports = app;
---
+++ tests/student-test.js
@@ -0,0 +1,94 @@
+const sinon = require('sinon');
+
+const mongoose = require('mongoose');
+mongoose.set('debug', true);
+require('sinon-mongoose');
+
+const app = require('../index.js');
+
+const agent = require('supertest').agent(app);
+
+const expect = require('chai').expect;
+
+const Student = mongoose.model('Student');
+
+var studentMock = sinon.mock(Student);
+
+beforeEach(() => {
+	console.log('beforeeach');
+	studentMock.restore();
+});
+
+afterEach(() => {
+	console.log('aftereach');
+	studentMock.verify();
+});
+
+describe('Bong-bong integrations tests', () => {
+	
+	const request = {
+		"students":
+		{
+			"address": 
+			{
+			"road":"PG 12",
+			"zipcode":"350 50",
+			"city":"Helsingborg"
+			},
+			"_id":"1z",
+			"email":"jonas@gmail.com",
+			"name":"Jonas"
+		}
+	};
+	
+	const expected = {
+		"students":
+		{
+			"address":
+			{
+				"road":"PG 12",
+				"zipcode":"350 50",
+				"city":"Helsingborg"
+			},
+				"_id":"1z",
+				"email":"jonas@gmail.com",
+				"name":"Jonas"
+		},
+				"_id":"5cee41f900086810b836b19a",
+				"__v":0
+				};
+				
+	describe('student.get', () => {
+		
+		it('Should return an array of all students', (done) => {
+			studentMock
+			.expects('find')
+			.chain('exec')
+			.resolves([expected]);
+			
+			agent
+			.get('/students')
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.eql([expected]);
+				done();
+			});
+		});
+		
+		it('Should return a student by name', (done) => {
+			studentMock
+			.expects('findOne')
+			.withArgs({"students.name": "Jonas"})
+			.chain('exec')
+			.resolves(expected);
+			
+			agent
+			.get('/students?name=Jonas')
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.eql(expected);
+				done();
+			});
+		});
+	});
+});
